7d0a55554f8d86650beae8d4875d4aa062e2770a
admin <730548326@qq.com>
2019-07-26 11:04:05 +0800

    通知

--- app/src/main/java/com/example/mvp/util/NoticetionUtil.java
+++ app/src/main/java/com/example/mvp/util/NoticetionUtil.java
@@ -1,11 +1,13 @@
 package com.example.mvp.util;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 
 import androidx.media.app.NotificationCompat;
 
@@ -18,18 +20,27 @@ import com.example.mvp.R;
  */
 public class NoticetionUtil {
 
-    public static void sendNotice(Intent intent){
-        NotificationManager manager= (NotificationManager) MyApplication.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            Notification notification=new Notification.Builder(MyApplication.getContext(),"FA").
-                    setLargeIcon(BitmapFactory.decodeResource(MyApplication.getContext().getResources(),R.mipmap.cat))
-                    .setSmallIcon(R.mipmap.bg1)
-                    .setContentText("消息通知...")
-                    .setContentTitle("通知")
-                    .setContentIntent(PendingIntent.getActivity(MyApplication.getContext(),1,intent,0))
-                    .build();
-            manager.notify(1,notification);
+    public static void getNotifition(String title,String contexttext,Intent intent) {
+        NotificationManager manager = (NotificationManager) MyApplication.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder notification ;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notification=new Notification.Builder(MyApplication.getContext(),"channel");
+        }else {
+            notification=new Notification.Builder(MyApplication.getContext());
         }
-
+        notification.setContentTitle(title);
+        notification .setContentText(contexttext);
+        notification .setWhen(System.currentTimeMillis());
+        notification  .setSmallIcon(R.mipmap.hread);
+        notification  .setLargeIcon(BitmapFactory.decodeResource(MyApplication.getContext().getResources(), R.mipmap.cat));
+        notification  .setAutoCancel(true);
+        //点击跳转
+        PendingIntent pendingIntent=PendingIntent.getActivity(MyApplication.getContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        notification.setContentIntent(pendingIntent);
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("channel", "测试", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+        manager.notify(1, notification.build());
     }
 }
